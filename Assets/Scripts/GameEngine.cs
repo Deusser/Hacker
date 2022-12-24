04acd807bcb37b815937394e8aa3f5fac20aca06
Aleksey <96611699+Deusser@users.noreply.github.com>
2022-12-24 21:50:07 +0300

    Initial commit

---
+++ Assets/Scripts/GameEngine.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEngine : MonoBehaviour
+{
+    public Text timerText;
+    public Text EndGameText;
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    private float gametime;
+    public Text pin1pintext;
+    public Text pin2pintext;
+    public Text pin3pintext;
+    public Text pin1currentpintext;
+    public Text pin2currentpintext;
+    public Text pin3currentpintext;
+    [SerializeField] GameObject TrojanButton;
+    [SerializeField] GameObject VirusButton;
+    [SerializeField] GameObject ScriptButton;
+    [SerializeField] GameObject EndingPanel;
+    [SerializeField] GameObject BadEnd;
+    [SerializeField] GameObject GoodEnd;
+    private int pin1cur;
+    private int pin2cur;
+    private int pin3cur;
+    private void Start()
+    {
+        pin1pintext.text = Random.Range(1, 5).ToString();
+        pin2pintext.text = Random.Range(1, 5).ToString();
+        pin3pintext.text = Random.Range(1, 5).ToString();
+        timerIsRunning = true;
+    }
+    public void Trojan()
+    {
+        pin1cur += 1;
+        pin2cur -= 1;
+        pin3cur += 0;
+    }
+    public void Virus()
+    {
+        pin1cur -= 1;
+        pin2cur += 2;
+        pin3cur -= 1;
+    }
+    public void Script()
+    {
+        pin1cur -= 1;
+        pin2cur += 1;
+        pin3cur += 1;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                pin1currentpintext.text = pin1cur.ToString();
+                pin2currentpintext.text = pin2cur.ToString();
+                pin3currentpintext.text = pin3cur.ToString();
+                gametime = Mathf.Round(timeRemaining);
+                timerText.text = gametime.ToString();
+                timeRemaining -= Time.deltaTime;
+                if (pin1currentpintext.text == pin1pintext.text && pin2currentpintext.text == pin2pintext.text && pin3currentpintext.text == pin3pintext.text)
+                {
+                    timerIsRunning = false;
+                    EndingPanel.SetActive(true);
+                    GoodEnd.SetActive(true);
+                }
+            }
+            else
+            {
+                EndGameText.text = ("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                EndingPanel.SetActive(true);
+                BadEnd.SetActive(true);
+            }
+        }
+    }
+}
